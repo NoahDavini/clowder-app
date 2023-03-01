00a25e208dd8d7ab5df74ec1a11bdbf06b308e74
nkelly3415 <nkelly3415@westfield.ma.edu>
2023-03-01 02:00:20 -0500

    24:33

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+//maybe don't need?
+type Props = {
+    children: React.ReactNode
+ }
+
+ const Layout: React.FC<Props> = ({ children }) => {
+//                      ^
+    return (
+        <>
+        <Navbar />
+        <main>{children}</main>
+        </>
+    );
+};
+export default Layout;
